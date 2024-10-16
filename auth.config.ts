a38d7cc188065a05d54d4db088be579b0ca98414
Yousef <y.majidin@gmail.com>
2024-10-16 14:19:47 -0400

    simplify authorized callback

--- auth.config.ts
+++ auth.config.ts
@@ -5,13 +5,7 @@ export const authConfig = {
         signIn: "/login",
     },
     callbacks: {
-        authorized({
-            auth,
-            request: { nextUrl },
-        }: {
-            auth: { user?: unknown };
-            request: { nextUrl: URL };
-        }) {
+        authorized({ auth, request: { nextUrl } }) {
             const isLoggedIn = !!auth?.user;
             const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
 
