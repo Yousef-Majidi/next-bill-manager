947cb47d0fbde3a3981d275407052db189032151
Yousef <y.majidin@gmail.com>
2024-12-04 17:41:36 -0500

    CRUD tenants implementation

--- app/dashboard/tenants/page.tsx
+++ app/dashboard/tenants/page.tsx
@@ -1,7 +1,44 @@
-export default function Page() {
+import { fetchTenantsPages } from "@/app/lib/data";
+import Pagination from "@/app/ui/tenants/pagination";
+import Search from "@/app/ui/search";
+import Table from "@/app/ui/tenants/table";
+import { CreateTenant } from "@/app/ui/tenants/buttons";
+import { Metadata } from "next";
+import { Suspense } from "react";
+import { TenantsTableSkeleton } from "@/app/ui/skeletons";
+
+export const metadata: Metadata = {
+    title: "Tenants",
+    description: "List of all tenants",
+};
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: { query?: string; page?: string };
+}) {
+    const query = searchParams?.query || "";
+    const currentPage = Number(searchParams?.page || 1);
+    const totalPages = await fetchTenantsPages(query);
+
     return (
-        <>
-            <h1 className={`mb-4 text-xl md:text-2xl`}>Tenants</h1>
-        </>
+        <div className="w-full">
+            <div className="flex w-full items-center justify-between">
+                <h1 className={`mb-4 text-xl md:text-2xl`}>Tenants</h1>
+            </div>
+            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+                <Search placeholder="Search tenants..." />
+                <CreateTenant />
+            </div>
+            <Suspense
+                key={query + currentPage}
+                fallback={<TenantsTableSkeleton />}
+            >
+                <Table query={query} currentPage={currentPage} />
+            </Suspense>
+            <div className="mt-5 flex w-full justify-center">
+                <Pagination totalPages={totalPages} />
+            </div>
+        </div>
     );
 }
---
+++ app/lib/definitions.ts
@@ -0,0 +1,7 @@
+export type Tenant = {
+    id: string;
+    email: string;
+    name: string;
+    hydro_share: number;
+    enbridge_share: number;
+};
