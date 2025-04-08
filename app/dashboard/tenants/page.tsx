37c9c31fea044c6a0f542eb053e8af54f20d8086
Yousef <y.majidin@gmail.com>
2025-04-08 12:09:54 -0400

    Refactor tenant management components and server actions for improved readability and consistency

--- app/dashboard/tenants/page.tsx
+++ app/dashboard/tenants/page.tsx
@@ -8,37 +8,37 @@ import { Suspense } from "react";
 import { TenantsTableSkeleton } from "@/app/ui/skeletons";
 
 export const metadata: Metadata = {
-    title: "Tenants",
-    description: "List of all tenants",
+	title: "Tenants",
+	description: "List of all tenants",
 };
 
 export default async function Page({
-    searchParams,
+	searchParams,
 }: {
-    searchParams?: { query?: string; page?: string };
+	searchParams?: { query?: string; page?: string };
 }) {
-    const query = searchParams?.query || "";
-    const currentPage = Number(searchParams?.page || 1);
-    const totalPages = await fetchTenantsPages(query);
+	const query = searchParams?.query || "";
+	const currentPage = Number(searchParams?.page || 1);
+	const totalPages = await fetchTenantsPages(query);
 
-    return (
-        <div className="w-full">
-            <div className="flex w-full items-center justify-between">
-                <h1 className={`mb-4 text-xl md:text-2xl`}>Tenants</h1>
-            </div>
-            <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-                <Search placeholder="Search tenants..." />
-                <CreateTenant />
-            </div>
-            <Suspense
-                key={query + currentPage}
-                fallback={<TenantsTableSkeleton />}
-            >
-                <Table query={query} currentPage={currentPage} />
-            </Suspense>
-            <div className="mt-5 flex w-full justify-center">
-                <Pagination totalPages={totalPages} />
-            </div>
-        </div>
-    );
+	return (
+		<div className="w-full">
+			<div className="flex w-full items-center justify-between">
+				<h1 className={`mb-4 text-xl md:text-2xl`}>Tenants</h1>
+			</div>
+			<div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+				<Search placeholder="Search tenants..." />
+				<CreateTenant />
+			</div>
+			<Suspense
+				key={query + currentPage}
+				fallback={<TenantsTableSkeleton />}
+			>
+				<Table query={query} currentPage={currentPage} />
+			</Suspense>
+			<div className="mt-5 flex w-full justify-center">
+				<Pagination totalPages={totalPages} />
+			</div>
+		</div>
+	);
 }
