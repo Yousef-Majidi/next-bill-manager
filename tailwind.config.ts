7fe9ae412bba028c7668f9acb0b396edb8e61fb6
Yousef <y.majidin@gmail.com>
2025-05-24 16:23:38 -0400

    initial commit: project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+	content: ["./src/**/*.{js,ts,jsx,tsx}", "./src/app/global.css"],
+	plugins: [],
+};
+
+export default config;
